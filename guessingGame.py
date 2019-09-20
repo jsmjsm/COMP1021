de6b56addae6f845be32785741a27a6ba2f356b7
Sahil Daswani <sahildaswani@Sahils-MacBook-Pro-2.local>
2019-09-21 00:04:43 +0800

    Added Turtle Graphics

--- guessingGame.py
+++ guessingGame.py
@@ -1,4 +1,5 @@
 import random
+import turtle
 
 # Generates random number in variable num
 num = random.randint(1,100)
@@ -9,18 +10,30 @@ answerState = False
 # +1 everytime a player makes a guess
 numberOfAttempts = 0
 
-while answerState == False:
+# Create Turtle
+text = turtle.Turtle()
+text.hideturtle()
+text.write("Please enter an integer number between 1 and 100.", align=("center"),font=("Arial", 20, "bold"))
+
+# Function to clear turtle and change text
+def ChangeText(message):
+    text.clear()
+    text.write(message, align=("center"), font=("Arial", 20, "bold"))
+
+while not answerState:
     # Input is created everytime the player gets the answer wrong
-    userNumber = int(input("Please enter a number between 0 and 100: "))
+    userNumber = int(turtle.textinput("Guessing Game", "Please enter a number between 1 and 100:"))
 
     # Game logic
     if userNumber == num:
         answerState = True
         numberOfAttempts += 1
-        print("Well done! The number was %s. You guessed it in %s tries." %(num, numberOfAttempts))
+        ChangeText("Well done! The number was %s. You guessed it in %s tries." % (num, numberOfAttempts))
     elif userNumber > num:
-        print("Too high")
         numberOfAttempts += 1
+        ChangeText("Too High.")
     elif userNumber < num:
-        print("Too low")
         numberOfAttempts += 1
+        ChangeText("Too Low.")
+
+turtle.done()
