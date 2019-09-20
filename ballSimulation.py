6066e43916907762992964c6d8b9fa85817f70f7
Sahil Daswani <sahildaswani@Sahils-MacBook-Pro-2.local>
2019-09-21 01:52:11 +0800

    Added speed controls and improved inelastic collision

--- ballSimulation.py
+++ ballSimulation.py
@@ -7,14 +7,37 @@ wn.bgcolor('black')
 wn.title("Bouncing Ball Simulation")
 wn.tracer(0)
 
+# Instructions Text
+text = turtle.Turtle()
+text.penup()
+text.color('white')
+text.goto(0,270)
+text.hideturtle()
+text.write("Press Spacebar to Speed Up Balls", align=("center"), font=("Arial", 15, "bold"))
+
+# Allows users to input the number of balls
 ballNum = int(turtle.textinput("Simulator Settings", "Please enter the number of balls:"))
 
+# Arrays and constants
 balls = []
 colors = ["red", "green", "blue", "white", "purple", "yellow"]
 
+gravity = 0.1
+
+# Increase speed when spacebar is pressed
+def SpeedUp():
+    for ball in balls:
+        ball.dy += random.randint(2,4)
+        ball.dx += 0.1
+
+wn.onkey(SpeedUp, 'space')
+wn.listen()
+
+# Create i number of balls
 for i in range(ballNum):
     balls.append(turtle.Turtle())
 
+# Ball properties
 for ball in balls:
     ball.color(colors[random.randint(0,len(colors)-1)])
     ball.penup()
@@ -30,27 +53,33 @@ for ball in balls:
     else:
         ball.dx = dx
 
-gravity = 0.1
-
+# Infinite loop
 while True:
     wn.update()
 
     for ball in balls:
+        # Gravity affecting vertical speed
         ball.dy -= gravity
-        ball.sety(ball.ycor() + ball.dy)
 
+        # X and Y position changing based of horizontal and vertical speed
+        ball.sety(ball.ycor() + ball.dy)
         ball.setx(ball.xcor() + ball.dx)
 
+        # Change direction when hitting a corner and decrease speed by 10%
         if ball.ycor() < -270:
-            ball.dy *= -0.95
+            ball.dy *= -0.9
             ball.sety(-270)
 
+        if ball.ycor() > 270:
+            ball.dy *= -0.9
+            ball.sety(270)
+
         if ball.xcor() > 300:
-            ball.dx *= -0.95
+            ball.dx *= -0.9
             ball.setx(300)
 
         if ball.xcor() < -300:
-            ball.dx *= -0.95
+            ball.dx *= -0.9
             ball.setx(-300)
 
 screen.mainloop()
